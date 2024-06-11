8d7ce6f9a3069352460c422e6570adb66f5c0113
Adão Araujo <adaoma11@hotmail.com>
2024-06-11 14:18:42 -0300

    feat: add pets/myadoptions route and getAllUserAdoptions to return all pets adopted by the user

--- controllers/PetController.js
+++ controllers/PetController.js
@@ -91,4 +91,15 @@ module.exports = class PetController {
 
         res.status(200).json({ pets });
     }
+
+    static async getAllUserAdoptions(req, res) {
+        const token = getToken(req);
+        const user = await getUserByToken(token);
+
+        const pets = await Pet.find({ "adopter._id": user._id }).sort(
+            "-createdAt"
+        );
+
+        res.status(200).json({ pets });
+    }
 };
--- routes/PetRoutes.js
+++ routes/PetRoutes.js
@@ -12,7 +12,8 @@ router.post(
     PetController.create
 );
 
-router.get("/mypets", verifyToken, PetController.getAllUserPets);
 router.get("/", PetController.getAll);
+router.get("/mypets", verifyToken, PetController.getAllUserPets);
+router.get("/myadoptions", verifyToken, PetController.getAllUserAdoptions);
 
 module.exports = router;
